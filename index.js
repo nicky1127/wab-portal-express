8b64e7972199a9aa2a37eb6ed9128fa95545d24e
Nicky Lai <nicky.lai@lloydsbanking.com>
2020-10-25 19:11:43 +0000

    Create route for auth

--- index.js
+++ index.js
@@ -13,12 +13,45 @@ const Auth0Strategy = require("passport-auth0");
 
 require("dotenv").config();
 
+const authRouter = require("./auth");
 /**
  * App Variables
  */
 
 const app = express();
 const port = process.env.PORT || "8000";
+
+/**
+ * Session Configuration
+ */
+
+const session = {
+  secret: process.env.SESSION_SECRET,
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+};
+
+if (app.get("env") === "production") {
+  // Serve secure cookies, requires HTTPS
+  session.cookie.secure = true;
+}
+
+/**
+ * Passport Configuration
+ */
+
+const strategy = new Auth0Strategy(
+  {
+    domain: process.env.AUTH0_DOMAIN,
+    clientID: process.env.AUTH0_CLIENT_ID,
+    clientSecret: process.env.AUTH0_CLIENT_SECRET,
+    callbackURL: process.env.AUTH0_CALLBACK_URL,
+  },
+  function (accessToken, refreshToken, extraParams, profile, done) {
+    return done(null, profile);
+  }
+);
 
 /**
  *  App Configuration
@@ -28,16 +61,54 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
 app.use(express.static(path.join(__dirname, "public")));
 
+app.use(expressSession(session));
+
+passport.use(strategy);
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser((user, done) => {
+  done(null, user);
+});
+
+passport.deserializeUser((user, done) => {
+  done(null, user);
+});
+
+// Creating custom middleware with Express
+app.use((req, res, next) => {
+  // console.log("req.isAuthenticated()", req.isAuthenticated());
+  // console.log("req.session: ", req.session);
+  // console.log("req.user: ", req.user);
+  res.locals.isAuthenticated = req.isAuthenticated();
+  next();
+});
+
+// Router mounting
+
+app.use("/", authRouter);
+
 /**
  * Routes Definitions
  */
+
+const secured = (req, res, next) => {
+  if (req.user) {
+    return next();
+  }
+  req.session.returnTo = req.originalUrl;
+  res.redirect("/login");
+};
 
 app.get("/", (req, res) => {
   res.render("index", { title: "Home" });
 });
 
-app.get("/user", (req, res) => {
-  res.render("user", { title: "Profile", userProfile: { nickname: "Auth0" } });
+app.get("/user", secured, (req, res) => {
+  res.render("user", {
+    title: "Profile",
+    userProfile: { nickname: req.user.displayName },
+  });
 });
 
 /**
